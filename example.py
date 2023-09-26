e63ef63f26dee666bf8489b32f1c01a9c8d1d95f
Jcwscience <jcwscience@me.com>
2023-09-26 18:35:20 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,17 +1,28 @@
+import bluetooth
 import random
 import time
-from ble_data_transfer import BLEServer
+from ble_data_transfer import BLEManager
 from pack_data import pack_data
 
 
-pico_server = BLEServer(name="Pi-Pico")
 
-service1 = pico_server.addService(description="DataTransfer", uuid="2e1973f2-558a-11ee-8c99-0242ac120002")
+_FLAG_READ = const(0x0002)
+_FLAG_NOTIFY = const(0x0010)
+_FLAG_INDICATE = const(0x0020)
 
-characteristic1 = service1.addCharacteristic(description="Random", uuid="2e197adc-558a-11ee-8c99-0242ac120002", flags=["read", "notify"])
+_SERVICE_UUID = bluetooth.UUID("c04cd54e-6c04-4fa2-8afb-93ac92e4a8fe")
+
+_CHARACTERISTIC = (bluetooth.UUID("1dc2422c-b770-4125-acc7-a5be80b6565e"),
+_FLAG_READ | _FLAG_NOTIFY | _FLAG_INDICATE,)
+
+_SERVICE = (_SERVICE_UUID,(_CHARACTERISTIC,),)
+
+
+manager = BLEManager(ble=bluetooth.BLE(), name="Pi-Pico", service=_SERVICE)
 
 while True:
-    random_number = random.randint(0, 100)
-    packed_data = pack_data(random_number)
-    characteristic1.update(packed_data)
-    time.sleep(1)
+    packed_int = pack_data(random.randint(0,100))
+    packed_string = pack_data("Test String")
+    manager.update_data(packed_int, notify=True, indicate=False)
+    manager.update_data(packed_string, notify=True, indicate=False)
+    time.sleep_ms(1000)
